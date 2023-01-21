6dce8e1f061e236ece255adeb6e19f1470b4de49
v1ab1 <sharkandmouse@gmail.com>
2023-01-21 16:43:09 +0500

    work on tg

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InputFile
+
 
 API_TOKEN = ''
 
@@ -12,14 +14,15 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Привет, я бот, который покажет тебя самую актуальную информацию о рынке воды в Челябинске! \nХочешь получить актуальную сводку?")
-
+    await message.answer("Привет, я бот, который покажет тебя самую актуальную информацию о рынке воды в Челябинске! \nХочешь получить актуальную сводку?")
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer('Привет, сейчас подготовим для тебя актуальный анализ рынка!')
+    await message.answer('Сейчас подготовим для тебя актуальный анализ рынка!')
     await message.answer('Подождите...')
+    await bot.send_document(message.from_user.id, open('analysis.xlsx', 'rb'))
+    await message.answer('Готово! :)')
 
 
 
