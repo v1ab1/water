f52c9e8b2fb31855189c678148ec91f94fded8ea
v1ab1 <sharkandmouse@gmail.com>
2023-01-21 16:21:36 +0500

    tg bot added

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from aquamobil import parser as aquamobil
-from chebarkul import parser as chebarkul
-from gorny import parser as gorny
-from lubima import parser as lubima
-from vlasov import parser as vlasov
-# from zhivaya import parser as zhivaya
-from prettytable import PrettyTable
-funclist = [aquamobil, chebarkul, gorny, lubima, vlasov]
-complist = ["aquamobil", "chebarkulsky istok", "gorny", "lubima", "vlasov kluch"]
-pricelist = []
-for func in funclist:
-    pricelist.append(int(func()))
-outTable = PrettyTable()
-outTable.field_names = [' '] + complist
-outTable.add_row(['price'] + pricelist)
-print(outTable)
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = ''
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет, я бот, который покажет тебя самую актуальную информацию о рынке воды в Челябинске! \nХочешь получить актуальную сводку?")
+
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer('Привет, сейчас подготовим для тебя актуальный анализ рынка!')
+    await message.answer('Подождите...')
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
