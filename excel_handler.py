c2a0b3fb2183dfde45da069acb72be4d178ee078
v1ab1 <sharkandmouse@gmail.com>
2023-01-23 22:57:48 +0500

    work on excel

--- excel_handler.py
+++ excel_handler.py
@@ -3,11 +3,12 @@ from openpyxl import load_workbook
 
 def excel_handle():
     wb = load_workbook('./analysis.xlsx')
-    print(wb.get_sheet_names())
     month = datetime.now().month
     month = month if len(f'{month}') >= 2 else f'0{month}'
     year = f'{datetime.now().year}'[-2:]
     date = f'{datetime.now().day}.{month}.{year}'
-    print(date)
+    if date in wb == False:
+        return
+    
 
 excel_handle()
