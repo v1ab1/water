1bb8e751623b129c8e0bfed38199c759c6e4b3bb
v1ab1 <sharkandmouse@gmail.com>
2023-01-25 19:49:17 +0500

    add buttons

--- main.py
+++ main.py
@@ -11,13 +11,7 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-
 update = False
-
-# buttonAnalisys = InlineKeyboardButton("Показать актуальный анализ", callback_data="show_analysis")
-# buttonUpdate = InlineKeyboardButton("Вкл. автообновление", callback_data="enable_auto_update")
-# buttonDiscounts = InlineKeyboardButton("Показать скидки", callback_data="show_discounts")
-# keyboard = InlineKeyboardMarkup().add(buttonAnalisys, buttonUpdate, buttonDiscounts)
 
 kb = [
     [
@@ -33,21 +27,42 @@ kb = [
 
 keyboard = InlineKeyboardMarkup(inline_keyboard=kb)
 
+yes = [
+    [
+        InlineKeyboardButton("Да", callback_data="yes"),
+    ],
+]
+
+yes_button = InlineKeyboardMarkup(inline_keyboard=yes)
+
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.answer("Привет, я бот, который покажет тебя самую актуальную информацию о рынке воды в Челябинске! \nХочешь получить сводку?")
+    await message.answer("Привет, я бот, который покажет тебя самую актуальную информацию о рынке воды в Челябинске! \nХочешь получить сводку?", reply_markup=yes_button)
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.answer("Выберите вариант!", reply_markup=keyboard)
-    # await message.answer('Сейчас подготовим для тебя актуальный анализ рынка!')
-    # await message.answer('Подождите...')
-    # await excel_handle()
-    # await bot.send_document(message.from_user.id, open('analysis.xlsx', 'rb'))
-    # await message.answer('Готово! :)')
 
+@dp.callback_query_handler()
+async def process_callback_button(callback_query: types.CallbackQuery):
+    if callback_query.data == 'analysis':
+        await bot.send_message(callback_query.from_user.id, 'Сейчас подготовим для тебя актуальный анализ рынка!')
+        await bot.send_message(callback_query.from_user.id, 'Подождите...')
+        await excel_handle()
+        await bot.send_document(callback_query.from_user.id, open('analysis.xlsx', 'rb'))
+        await bot.send_message(callback_query.from_user.id, 'Готово! :)')
+        pass
+    elif callback_query.data == 'discounts':
+        # Ваш код для выполнения функции discounts()
+        pass
+    elif callback_query.data == 'auto_update':
+        # Ваш код для выполнения функции auto_update()
+        pass
+    elif callback_query.data == 'yes':
+        await bot.send_message(callback_query.from_user.id, "Выберите вариант!", reply_markup=keyboard)
+        pass
 
 # async def excel_update():
 #     if update:
